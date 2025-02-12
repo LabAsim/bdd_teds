f8525e2429389f56a0d65866ea308b64fcf1cda8
LabAsim <lampgit@pm.me>
2025-02-12 02:43:09 +0200

    commit helper funcs

---
+++ helper.R
@@ -0,0 +1,72 @@
+library(tidyverse)
+
+test <- df
+
+fill_single_var_twin_from_cotwin <- function(df, var){
+  calculate <- data.frame(f_id=1)
+  for (f_id in unique(getElement(df, "fam_id"))){
+    twin_df <- df[getElement(df, "fam_id")==f_id,]
+    if (
+      # is.numeric returns T even though it's NA
+      # See: https://statisticaloddsandends.wordpress.com/2019/04/09/testing-numeric-variables-for-na-nan-inf/
+      (is.na(twin_df[1,var]) == T & !is.na(twin_df[2,var]) == T) | 
+      (!is.na(twin_df[1,var])== T & is.na(twin_df[2,var])== T)
+    ){
+      if (is.na(twin_df[1,var]) == T){
+        twin_df[1,var] <- twin_df[2,var]
+      }else{
+        twin_df[2,var] <- twin_df[1,var]
+      }
+      df[getElement(df, "fam_id")== f_id &  getElement(df, "twin_id")==twin_df[1,"twin_id"],var] <- twin_df[1,var]
+      df[getElement(df, "fam_id")== f_id &  getElement(df, "twin_id")==twin_df[2,"twin_id"],var] <- twin_df[2,var]
+      calculate <- rbind(calculate, data.frame(f_id))
+    }
+
+  }
+  calculate <- data.frame(f_id=calculate[2:nrow(calculate),])
+  print(glue::glue(paste("Found {dim(calculate)[1]} pair(s) for '{var}'!")))
+  return(df)
+}
+
+# Test it
+# A co-twin of this pair had NA in MPVS
+# However, note that
+# we need this function only for AGE, which is the same for the twins,
+# but not for the rest vars, in which they may differ naturally
+s <- fill_single_var_twin_from_cotwin(df=test[1:150,], var="mpvs_total_12_1")
+stopifnot(all.equal(s[80,"mpvs_total_12_1"], s[79,"mpvs_total_12_1"]))
+
+
+fill_multiple_vars_twin_from_cotwin <- function(df,vars){
+  
+  for (var in vars){
+    df <- fill_single_var_twin_from_cotwin(df=df, var=var)
+  }
+  return(df)
+}
+
+# Test it
+s <- fill_multiple_vars_twin_from_cotwin(
+  df=test[1:150,], 
+  vars=c("mpvs_total_12_1", "mpvs_physical_12_1", "mpvs_physical_12_2")
+)
+
+stopifnot(all.equal(s[80,"mpvs_total_12_1"], s[79,"mpvs_total_12_1"]))
+stopifnot(all.equal(s[80,"mpvs_physical_12_1"], s[79,"mpvs_physical_12_1"]))
+stopifnot(all.equal(s[80,"mpvs_physical_12_2"], s[79,"mpvs_physical_12_2"]))
+
+s <- fill_multiple_vars_twin_from_cotwin(
+  df=test[1:150,], 
+  vars=c(colnames(df)[grepl(pattern="age", x=colnames(df))])
+)
+stopifnot(all.equal(s[1,"age_26_1"], s[2,"age_26_1"]))
+stopifnot(all.equal(s[1,"age_26_2"], s[2,"age_26_2"]))
+
+
+# Remove test objects
+rm(list=c("test", "s"))
+
+
+
+
+
