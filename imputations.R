19f4a3e69247a76f447bdb2cf14247042f7f837d
LabAsim <lampgit@pm.me>
2025-04-08 11:16:07 +0100

    play a sound after completing time-consuming MI

--- imputations.R
+++ imputations.R
@@ -1,6 +1,7 @@
 library(mice)
 library(miceadds)
 library(micemd)
+library(beepr)
 
 SEED <- 123
 set.seed(seed = SEED)
@@ -121,13 +122,16 @@ impute_items <- function(df,parallel=T, maxit=1, m=1,n.core=1){
   # Remove collinear vars #
   #########################
   predMatrix["age_parent_12",c("age_teach_14_1", "age_child_14_1","age_parent_14","age_web_16_1")] <- 0
+  predMatrix["age_child_12_1",c("age_teach_14_1", "age_child_14_1","age_parent_14","age_web_16_1")] <- 0
+  
   for (var in 1:16){
     predMatrix[paste0("mpvs_item_",var,"_14_teacher_1"),colnames(df_imp)[grepl(pattern="21",x=colnames(df_imp))]] <- 0
   }
   predMatrix["age_teach_14_1",] <- 0
   predMatrix["age_teach_14_1","fam_id"] <- -2
-  predMatrix["age_teach_14_1", c("sex_1", colnames(df_imp)[grepl(pattern="age",x=colnames(df_imp))])]
+  predMatrix["age_teach_14_1", c("sex_1", colnames(df_imp)[grepl(pattern="age",x=colnames(df_imp))])] <- 1
   predMatrix["age_teach_14_1","age_teach_14_1"] <- 0
+  predMatrix["age_parent_16", c(colnames(df_imp)[grepl(pattern="16", x=colnames(df_imp))])] <- 0
   View(predMatrix)
   #############
   # Call mice #
@@ -140,80 +144,31 @@ impute_items <- function(df,parallel=T, maxit=1, m=1,n.core=1){
       m = m, levels_id = cluster, variables_levels = level, seed=SEED,
       post=post
     )
-    data <- complete(imp)
-    print(Sys.time() - startTime)
-    summary(data)
-    return(data)
+  }else if(parallel == T){
+    # Run in parallel
+    print(parallelly::availableCores(logical = TRUE))
+    imp <- futuremice(
+      df_imp, method = impMethod, predictorMatrix = predMatrix, maxit = maxit,
+      m = m, levels_id = cluster, variables_levels = level, parallelseed=SEED,
+      post=post, n.core = n.core,
+      print=T
+    )
+    beep("mario")
+    Sys.sleep(0.5)
   }
-  
-  # Run in parallel
-  print(parallelly::availableCores(logical = TRUE))
-  imp <- futuremice(
-    df_imp, method = impMethod, predictorMatrix = predMatrix, maxit = maxit,
-    m = m, levels_id = cluster, variables_levels = level, parallelseed=SEED,
-    post=post, n.core = n.core,
-    print=T
-  )
   print(Sys.time() - startTime)
   return(imp)
 }
 
 
-imp <- impute_items(df=df_1, parallel=T, maxit=5, m=14, n.core=14)
+# imp <- impute_items(df=df_1, parallel=T, maxit=5, m=14, n.core=14)
 
-# imp <- impute_items(df=df_1,parallel=F, maxit=5, m=15)
-s
+imp <- impute_items(df=df_1,parallel=F, maxit=1, m=1)
 data_imp <- complete(imp)
 summary(data_imp)
 
 summary(data_imp$mpvs_item_2_12_1)
 data_imp[data_imp$mpvs_item_2_12_1>2,"mpvs_item_2_12_1"]
-
-calculate_totals <- function(df){
-  
-  df$mpvs_total_12_1 <- rowSums(
-    x=df[,c(
-      "mpvs_physical_12_1",
-      "mpvs_verbal_12_1",
-      "mpvs_social_12_1",
-      "mpvs_property_12_1"
-    )],
-    na.rm = F
-  )
-  
-  
-  df$mpvs_total_14_parent_1 <- rowSums(
-    x=df[,c(
-      "mpvs_physical_14_parent_1",
-      "mpvs_verbal_14_parent_1",
-      "mpvs_social_14_parent_1",
-      "mpvs_property_14_parent_1"
-    )]
-  )
-  
-  
-  df$mpvs_total_14_child_1 <- rowSums(
-    x=df[,c(
-      "mpvs_physical_14_child_1",
-      "mpvs_verbal_14_child_1",
-      "mpvs_social_14_child_1",
-      "mpvs_property_14_child_1"
-    )]
-  )
-  
-  
-  df$mpvs_total_14_teacher_1 <- rowSums(
-    x=df[,c(
-      "mpvs_physical_14_teacher_1",
-      "mpvs_verbal_14_teacher_1",
-      "mpvs_social_14_teacher_1",
-      "mpvs_property_14_teacher_1"
-    )]
-  )
-  
-}
-
-
 
 # Recalculate all total items
 data_imp_calc <- data_imp %>%
@@ -228,4 +183,3 @@ data_imp_calc <- data_imp %>%
   calculate_items(target_phrase = "_21_cov3_2_1", output_var = "mpvs_total_21_cov3_1") %>%
   calculate_items(target_phrase = "_21_cov4_2_1", output_var = "mpvs_total_21_cov4_1")%>%
   calculate_items(target_phrase = "_26_1", output_var = "dcq_26_1")
-
