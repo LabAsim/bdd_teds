0ee694c9f2ef300803b42f06cf8ae85d282f5927
LabAsim <lampgit@pm.me>
2025-05-01 17:33:32 +0100

    mpvs density plot, MPVS means

--- plots.R
+++ plots.R
@@ -1,6 +1,40 @@
 library(ggplot2)
 
 
+density_mpvs_scaled_32 <- ggplot(
+  data=df_all_diffs_long,
+  aes(x=mpvs)
+) + 
+  geom_density(aes(color=mpvs_age,group = mpvs_age)) + 
+  labs(
+    y = "Density",
+    x = "ΜΖ difference in MPVS score",
+    title = "ΜΖ difference in MPVS (scaled at 32 items)"
+  ) +
+  theme_minimal() +
+  theme(
+    axis.title.x = element_text(hjust = 0.5),
+    plot.title = element_text(hjust = 0.5)
+  ) 
+# density_mpvs_scaled_32
+
+density_dcq <- ggplot(
+  data=df_all_diffs_long,
+  aes(x=dcq_total_26_1)
+) + 
+  geom_density() + 
+  labs(
+    y = "Density",
+    x = "ΜΖ difference in DCQ score",
+    title = "DCQ diff"
+  ) +
+  theme_minimal() +
+  theme(
+    axis.title.x = element_text(hjust = 0.5),
+    plot.title = element_text(hjust = 0.5)
+  ) 
+# density_dcq
+
 longitudinal_means <- ggplot(
     data=summary_df, 
     aes(
@@ -23,8 +57,28 @@ longitudinal_means <- ggplot(
   scale_x_continuous(breaks = c(seq(12, 22, 2)), limits = c(12, 22))
 
 
-longitudinal_means_scaled <- ggplot(
-  data=summary_scaled_df, 
+longitudinal_means_scaled_without_NA_21 <- ggplot(
+  data=summary_df_without_NA_21, 
+  aes(
+    x=age, y=mean, color = informant,shape = informant
+  )
+) + geom_point() + geom_line() +
+  labs(
+    y = "Mean",
+    x = "Age",
+    title = "MPVS (scaled at maximum of 32-points)"
+  ) +
+  theme_minimal() +
+  theme(
+    # axis.text.x = element_blank(),
+    # axis.ticks.x = element_text(hjust = 0.5),
+    axis.title.x = element_text(hjust = 0.5),
+    plot.title = element_text(hjust = 0.5)
+  ) 
+
+
+longitudinal_means_scaled_without_NA <- ggplot(
+  data=summary_df_without_NA, 
   aes(
     x=age, y=mean, color = informant,shape = informant
   )
@@ -43,6 +97,7 @@ longitudinal_means_scaled <- ggplot(
   ) +
   scale_y_continuous(breaks = c(seq(0, 10, 1)), limits = c(0, 10))+
   scale_x_continuous(breaks = c(seq(12, 22, 2)), limits = c(12, 22))
+
 
 
 plot_mpvs <- function(df, var_to_plot, title, x_axis_title){
