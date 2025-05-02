fee04f2f34327525860a704fd4b0ee59b783b089
LabAsim <lampgit@pm.me>
2025-05-02 15:43:00 +0100

    source data scripts

--- descriptives.R
+++ descriptives.R
@@ -8,38 +8,15 @@ if (exists("df_1") == F){
 }
 
 if (exists("df_essential_vars") == F){
-  vars <- c(
-    "school_cohort_fct",
-    "fam_id",
-    "twin_id",
-    "sex_1",
-    "zygosity_binary_fct" ,
-    # Ses & ethnicity serve as auxiliary vars, 
-    # because they have many obs and are loosely associated with
-    # Bullying and BDD
-    "ses_1st_contact",
-    "ethnic_fct" ,
-    "age_parent_12",
-    "age_teach_12_1",
-    "age_child_12_1",
-    "age_parent_14",
-    "age_teach_14_1",
-    "age_child_14_1",
-    "age_child_web_16_1",
-    "age_phase1_parent_21",
-    "age_phase2_child_21_1",
-    "age_cov1_child_21_1",
-    "age_cov2_child_21_1",
-    "age_cov3_child_21_1",
-    "age_cov4_child_21_1",
-    "age_26_1",
-    colnames(df_1)[grepl(pattern="mpvs_total",x=colnames(df_1))],
-    "dcq_total_26_1"
-  )
-  
-  df_essential_vars <- df_1 %>% 
-    dplyr::select(all_of(vars)) 
+  source("data_management_hyp_1.R")
 }
+
+
+if (exists("df_all_diffs") == F){
+  source("data_management_hyp_2.R")
+}
+
+
 
 corr_mat <- cor(
   df_essential_vars %>% select(
@@ -408,7 +385,7 @@ summary_df_without_NA_21 <- create_summary_df(
     "mpvs_total_cov4_21_1_scaled_32"
   )
 )
-
+l
 summary_df_without_NA <- create_summary_df(
   df=df_without_NA,
   var_vec = c(
