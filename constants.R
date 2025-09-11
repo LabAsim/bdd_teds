7181aa39041c1e2ffc371e7e1dbc28e0b3f26ec8
LabAsim <lampgit@pm.me>
2025-09-11 22:59:01 +0300

    add whitespace

--- constants.R
+++ constants.R
@@ -1,2 +1,2 @@
 footnote <- "'MZ: Monozygotic, DZ: Dizygotic, DCQ: Dysmorphic concerns questionnaire, MPVS: Multidimensional peer victimization scale, y: year(s)
-    ***: p-value <=0.001, **: 0.001 < p-value <= 0.01,*: 0.01 < p-value <= 0.05'"
+    ***: p-value <=0.001, **: 0.001 < p-value <= 0.01, *: 0.01 < p-value <= 0.05'"
