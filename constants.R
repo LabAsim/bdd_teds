56e05f09c5b2d0271f37ca4a5f1fe35dc87faef5
LabAsim <lampgit@pm.me>
2025-09-12 21:29:38 +0300

    footnotes

--- constants.R
+++ constants.R
@@ -1,2 +1,4 @@
 footnote <- "'MZ: Monozygotic, DZ: Dizygotic, DCQ: Dysmorphic concerns questionnaire, MPVS: Multidimensional peer victimization scale, y: year(s)
     ***: p-value <=0.001, **: 0.001 < p-value <= 0.01, *: 0.01 < p-value <= 0.05'"
+footnote_acronyms <- "'MZ: Monozygotic, DZ: Dizygotic, DCQ: Dysmorphic concerns questionnaire, MPVS: Multidimensional peer victimization scale, y: year(s)'"
+footnote_acronyms_2 <- "N refers to individuals, MZ: Monozygotic, DZ: Dizygotic, DCQ: Dysmorphic concerns questionnaire, MPVS: Multidimensional peer victimization scale, y: year(s)"
