82646eb7c5b4872d1be6780855cd9d91715a28b1
LabAsim <lampgit@pm.me>
2025-09-23 16:13:55 +0300

    comment covid and MI models

--- analysis_hyp_2.R
+++ analysis_hyp_2.R
@@ -11,63 +11,63 @@ library(lavaan.mi)
 # Using original data #
 #######################
 
-# fiml
-twin_diff_model_with_covid_scaled <- "
-    dcq_total_26_1 ~ mpvs_total_12_1_scaled_32
-    dcq_total_26_1 ~ mpvs_total_child_14_1_scaled_32
-    dcq_total_26_1 ~ mpvs_total_16_1_scaled_32
-    dcq_total_26_1 ~ mpvs_total_phase_2_21_1_scaled_32
-    dcq_total_26_1 ~ mpvs_total_cov1_21_1_scaled_32
-    dcq_total_26_1 ~ mpvs_total_cov2_21_1_scaled_32
-    dcq_total_26_1 ~ mpvs_total_cov3_21_1_scaled_32
-    dcq_total_26_1 ~ mpvs_total_cov4_21_1_scaled_32
-    mpvs_total_child_14_1_scaled_32 ~ mpvs_total_12_1_scaled_32
-    mpvs_total_16_1_scaled_32 ~ mpvs_total_child_14_1_scaled_32
-    mpvs_total_phase_2_21_1_scaled_32 ~ mpvs_total_16_1_scaled_32
-    mpvs_total_cov1_21_1_scaled_32 ~ mpvs_total_phase_2_21_1_scaled_32
-    mpvs_total_cov2_21_1_scaled_32 ~ mpvs_total_cov1_21_1_scaled_32
-    mpvs_total_cov3_21_1_scaled_32 ~ mpvs_total_cov2_21_1_scaled_32
-    mpvs_total_cov4_21_1_scaled_32 ~ mpvs_total_cov3_21_1_scaled_32
-"
-
-fit_fiml_diff_model_with_covid_scaled <- sem(
-  model = twin_diff_model_with_covid_scaled, data = df_all_diffs,
-  missing = "fiml"
-)
-summary(fit_fiml_diff_model_with_covid_scaled)
-
-resid(fit_fiml_diff_model_with_covid_scaled, type = "cor.bollen")
-
-lavaanPlot::lavaanPlot(
-  model = fit_fiml_diff_model_with_covid_scaled,
-  edge_options = list(color = "grey"),
-  coefs = TRUE, covs = TRUE,
-  graph_options = list(rankdir = "LR")
-)
-
-# ml
-twin_diff_model_scaled <- "
-    dcq_total_26_1 ~ mpvs_total_12_1_scaled_32
-    dcq_total_26_1 ~ mpvs_total_child_14_1_scaled_32
-    dcq_total_26_1 ~ mpvs_total_16_1_scaled_32
-    dcq_total_26_1 ~ mpvs_total_phase_2_21_1_scaled_32
-    dcq_total_26_1 ~ mpvs_total_cov1_21_1_scaled_32
-    dcq_total_26_1 ~ mpvs_total_cov2_21_1_scaled_32
-    dcq_total_26_1 ~ mpvs_total_cov3_21_1_scaled_32
-    dcq_total_26_1 ~ mpvs_total_cov4_21_1_scaled_32
-    mpvs_total_child_14_1_scaled_32 ~ mpvs_total_12_1_scaled_32
-    mpvs_total_16_1_scaled_32 ~ mpvs_total_child_14_1_scaled_32
-    mpvs_total_phase_2_21_1_scaled_32 ~ mpvs_total_16_1_scaled_32
-    mpvs_total_cov1_21_1_scaled_32 ~ mpvs_total_phase_2_21_1_scaled_32
-    mpvs_total_cov2_21_1_scaled_32 ~ mpvs_total_cov1_21_1_scaled_32
-    mpvs_total_cov3_21_1_scaled_32 ~ mpvs_total_cov2_21_1_scaled_32
-    mpvs_total_cov4_21_1_scaled_32 ~ mpvs_total_cov3_21_1_scaled_32
-"
-
-fit_ml <- sem(
-  model = twin_diff_model_scaled, data = df_all_diffs
-)
-summary(fit_ml)
+# # fiml
+# twin_diff_model_with_covid_scaled <- "
+#     dcq_total_26_1 ~ mpvs_total_12_1_scaled_32
+#     dcq_total_26_1 ~ mpvs_total_child_14_1_scaled_32
+#     dcq_total_26_1 ~ mpvs_total_16_1_scaled_32
+#     dcq_total_26_1 ~ mpvs_total_phase_2_21_1_scaled_32
+#     dcq_total_26_1 ~ mpvs_total_cov1_21_1_scaled_32
+#     dcq_total_26_1 ~ mpvs_total_cov2_21_1_scaled_32
+#     dcq_total_26_1 ~ mpvs_total_cov3_21_1_scaled_32
+#     dcq_total_26_1 ~ mpvs_total_cov4_21_1_scaled_32
+#     mpvs_total_child_14_1_scaled_32 ~ mpvs_total_12_1_scaled_32
+#     mpvs_total_16_1_scaled_32 ~ mpvs_total_child_14_1_scaled_32
+#     mpvs_total_phase_2_21_1_scaled_32 ~ mpvs_total_16_1_scaled_32
+#     mpvs_total_cov1_21_1_scaled_32 ~ mpvs_total_phase_2_21_1_scaled_32
+#     mpvs_total_cov2_21_1_scaled_32 ~ mpvs_total_cov1_21_1_scaled_32
+#     mpvs_total_cov3_21_1_scaled_32 ~ mpvs_total_cov2_21_1_scaled_32
+#     mpvs_total_cov4_21_1_scaled_32 ~ mpvs_total_cov3_21_1_scaled_32
+# "
+#
+# fit_fiml_diff_model_with_covid_scaled <- sem(
+#   model = twin_diff_model_with_covid_scaled, data = df_all_diffs,
+#   missing = "fiml"
+# )
+# summary(fit_fiml_diff_model_with_covid_scaled)
+#
+# resid(fit_fiml_diff_model_with_covid_scaled, type = "cor.bollen")
+#
+# lavaanPlot::lavaanPlot(
+#   model = fit_fiml_diff_model_with_covid_scaled,
+#   edge_options = list(color = "grey"),
+#   coefs = TRUE, covs = TRUE,
+#   graph_options = list(rankdir = "LR")
+# )
+#
+# # ml
+# twin_diff_model_scaled <- "
+#     dcq_total_26_1 ~ mpvs_total_12_1_scaled_32
+#     dcq_total_26_1 ~ mpvs_total_child_14_1_scaled_32
+#     dcq_total_26_1 ~ mpvs_total_16_1_scaled_32
+#     dcq_total_26_1 ~ mpvs_total_phase_2_21_1_scaled_32
+#     dcq_total_26_1 ~ mpvs_total_cov1_21_1_scaled_32
+#     dcq_total_26_1 ~ mpvs_total_cov2_21_1_scaled_32
+#     dcq_total_26_1 ~ mpvs_total_cov3_21_1_scaled_32
+#     dcq_total_26_1 ~ mpvs_total_cov4_21_1_scaled_32
+#     mpvs_total_child_14_1_scaled_32 ~ mpvs_total_12_1_scaled_32
+#     mpvs_total_16_1_scaled_32 ~ mpvs_total_child_14_1_scaled_32
+#     mpvs_total_phase_2_21_1_scaled_32 ~ mpvs_total_16_1_scaled_32
+#     mpvs_total_cov1_21_1_scaled_32 ~ mpvs_total_phase_2_21_1_scaled_32
+#     mpvs_total_cov2_21_1_scaled_32 ~ mpvs_total_cov1_21_1_scaled_32
+#     mpvs_total_cov3_21_1_scaled_32 ~ mpvs_total_cov2_21_1_scaled_32
+#     mpvs_total_cov4_21_1_scaled_32 ~ mpvs_total_cov3_21_1_scaled_32
+# "
+#
+# fit_ml <- sem(
+#   model = twin_diff_model_scaled, data = df_all_diffs
+# )
+# summary(fit_ml)
 
 # Excluding COVID period #
 twin_diff_model_scaled_without_covid <- "
@@ -79,11 +79,6 @@ twin_diff_model_scaled_without_covid <- "
     mpvs_total_16_1_scaled_32 ~ mpvs_total_child_14_1_scaled_32
     mpvs_total_phase_2_21_1_scaled_32 ~ mpvs_total_16_1_scaled_32
 "
-# fit_ml_without_covid <- sem(
-#   model = twin_diff_model_scaled_without_covid, data = df_all_diffs
-# )
-# summary(fit_ml_without_covid)
-
 
 fit_fiml_without_covid <- sem(
   model = twin_diff_model_scaled_without_covid, data = df_all_diffs,
@@ -117,8 +112,7 @@ twin_diff_model_scaled_without_covid1 <- "
     mpvs_total_16_1_scaled_32  ~ mpvs_total_12_1_scaled_32
 
     # Another over-identified model could add this cov
-    # instead of the aforementioned
-    # Covs
+    # instead of the aforementioned parameters
     #mpvs_total_12_1_scaled_32  ~~ mpvs_total_phase_2_21_1_scaled_32
     #mpvs_total_12_1_scaled_32  ~~ mpvs_total_16_1_scaled_32
 
@@ -190,48 +184,48 @@ plot_fit_fiml_standardized
 ######################
 # Using imputed data #
 ######################
-if (sys.nframe() == 0) {
-  if (exists("imp_derived") == F) {
-    if (file.exists("G:\\imp_derived.Rdata")) {
-      load("G:\\imp_derived.Rdata")
-    } else {
-      source("imputation_derived.R")
-    }
-  }
-}
-if (sys.nframe() == 0) {
-  if (exists("imp_derived") == F) {
-    if (file.exists("G:\\imp_derived.Rdata")) {
-      load("G:\\imp_derived.Rdata")
-    } else {
-      source("imputation_derived.R")
-    }
-  }
-
-  data_imp_original <- imp_data_derived
-
-  data_imp_original <- lapply(
-    X = data_imp_original,
-    FUN = function(x) {
-      scale_mpvs(
-        df = x,
-        scale_size = 32,
-        from_vars = c(
-          "mpvs_total_12_1",
-          "mpvs_total_parent_14_1",
-          "mpvs_total_child_14_1",
-          "mpvs_total_teacher_14_1",
-          "mpvs_total_16_1",
-          "mpvs_total_phase_2_21_1",
-          "mpvs_total_cov1_21_1",
-          "mpvs_total_cov2_21_1",
-          "mpvs_total_cov3_21_1",
-          "mpvs_total_cov4_21_1"
-        )
-      )
-    }
-  )
-}
+# if (sys.nframe() == 0) {
+#   if (exists("imp_derived") == F) {
+#     if (file.exists("G:\\imp_derived.Rdata")) {
+#       load("G:\\imp_derived.Rdata")
+#     } else {
+#       source("imputation_derived.R")
+#     }
+#   }
+# }
+# if (sys.nframe() == 0) {
+#   if (exists("imp_derived") == F) {
+#     if (file.exists("G:\\imp_derived.Rdata")) {
+#       load("G:\\imp_derived.Rdata")
+#     } else {
+#       source("imputation_derived.R")
+#     }
+#   }
+#
+#   data_imp_original <- imp_data_derived
+#
+#   data_imp_original <- lapply(
+#     X = data_imp_original,
+#     FUN = function(x) {
+#       scale_mpvs(
+#         df = x,
+#         scale_size = 32,
+#         from_vars = c(
+#           "mpvs_total_12_1",
+#           "mpvs_total_parent_14_1",
+#           "mpvs_total_child_14_1",
+#           "mpvs_total_teacher_14_1",
+#           "mpvs_total_16_1",
+#           "mpvs_total_phase_2_21_1",
+#           "mpvs_total_cov1_21_1",
+#           "mpvs_total_cov2_21_1",
+#           "mpvs_total_cov3_21_1",
+#           "mpvs_total_cov4_21_1"
+#         )
+#       )
+#     }
+#   )
+# }
 ##############################
 # Create a df of differences #
 ##############################
