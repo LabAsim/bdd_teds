010b16d727ddda51daa33276bb409835d393f581
LabAsim <lampgit@pm.me>
2025-09-23 22:27:36 +0300

    names for vars

--- constants.R
+++ constants.R
@@ -2,3 +2,18 @@ footnote <- "'MZ: Monozygotic, DZ: Dizygotic, DCQ: Dysmorphic concerns questionn
     ***: p-value <=0.001, **: 0.001 < p-value <= 0.01, *: 0.01 < p-value <= 0.05'"
 footnote_acronyms <- "'MZ: Monozygotic, DZ: Dizygotic, DCQ: Dysmorphic concerns questionnaire, MPVS: Multidimensional peer victimization scale, y: year(s)'"
 footnote_acronyms_2 <- "N refers to individuals, MZ: Monozygotic, DZ: Dizygotic, DCQ: Dysmorphic concerns questionnaire, MPVS: Multidimensional peer victimization scale, y: year(s)"
+
+
+var_labels <- list(
+  "dcq_total_26_1" = "DCQ",
+  "mpvs_total_12_1_scaled_32" = "MPVS(12y)",
+  "mpvs_total_child_14_1_scaled_32" = "MPVS(14y)",
+  "mpvs_total_16_1_scaled_32" = "MPVS(16y)",
+  "mpvs_total_phase_2_21_1_scaled_32" = "MPVS(21y)",
+  "age_child_12_1" = "Age(12y)",
+  "age_child_14_1" = "Age(14y)",
+  "age_child_web_16_1" = "Age(16y)",
+  "age_phase2_child_21_1" = "Age(21y)",
+  "age_26_1" = "Age(26y)",
+  "sex_1_fct" = "Sex"
+)
