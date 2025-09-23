0b5507864555afe250dab30c141725e32507a717
LabAsim <lampgit@pm.me>
2025-09-23 17:32:22 +0300

    modified model added

--- analysis_sensitivity_hyp_1.R
+++ analysis_sensitivity_hyp_1.R
@@ -48,6 +48,57 @@ fit_fiml_scaled_32_without_covid_without_ED <- sem(
 )
 summary(fit_fiml_scaled_32_without_covid_without_ED, standardized = T)
 
+resid(fit_fiml_scaled_32_without_covid_without_ED, type = "cor.bollen")
+modindices(fit_fiml_scaled_32_without_covid_without_ED, sort. = T)
+
+
+
+model_scaled_32_without_covid_modified <- "
+    # DCQ
+    dcq_total_26_1 ~ mpvs_total_12_1_scaled_32
+    dcq_total_26_1 ~ mpvs_total_child_14_1_scaled_32
+    dcq_total_26_1 ~ mpvs_total_16_1_scaled_32
+    dcq_total_26_1 ~ mpvs_total_phase_2_21_1_scaled_32
+
+    # Mpvs
+    mpvs_total_child_14_1_scaled_32 ~ mpvs_total_12_1_scaled_32
+    mpvs_total_16_1_scaled_32 ~ mpvs_total_child_14_1_scaled_32
+    mpvs_total_phase_2_21_1_scaled_32 ~ mpvs_total_16_1_scaled_32
+
+    # Age
+    dcq_total_26_1 ~ age_26_1
+    mpvs_total_12_1_scaled_32 ~ age_child_12_1
+    mpvs_total_child_14_1_scaled_32 ~ age_child_14_1
+    mpvs_total_16_1_scaled_32 ~ age_child_web_16_1
+    mpvs_total_phase_2_21_1_scaled_32 ~ age_phase2_child_21_1
+
+    # Sex
+    dcq_total_26_1 ~ sex_1_fct
+    mpvs_total_12_1_scaled_32 ~ sex_1_fct
+    mpvs_total_child_14_1_scaled_32 ~ sex_1_fct
+    mpvs_total_16_1_scaled_32 ~ sex_1_fct
+    mpvs_total_phase_2_21_1_scaled_32 ~ sex_1_fct
+
+    # Added
+    mpvs_total_12_1_scaled_32 ~~ mpvs_total_16_1_scaled_32
+    mpvs_total_child_14_1_scaled_32 ~~ mpvs_total_16_1_scaled_32
+    mpvs_total_16_1_scaled_32   ~~ mpvs_total_phase_2_21_1_scaled_32
+"
+fit_fiml_scaled_32_without_covid_without_ED <- sem(
+  model = model_scaled_32_without_covid_modified,
+  data = df_essential_vars_without_ED,
+  cluster = "fam_id",
+  missing = "fiml"
+)
+summary(fit_fiml_scaled_32_without_covid_without_ED, standardized = T)
+
+resid(fit_fiml_scaled_32_without_covid_without_ED, type = "cor.bollen")
+modindices(fit_fiml_scaled_32_without_covid_without_ED, sort. = T)
+
+
+
+
+
 labels <- list(
   dcq_total_26_1 = "DCQ (26y)",
   mpvs_total_12_1_scaled_32 = "MPVS (12y)",
