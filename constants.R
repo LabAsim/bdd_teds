905e12b4a0a7d5d21963093df2272bcc1a802479
LabAsim <lampgit@pm.me>
2025-10-23 20:27:24 +0300

    update footnote

--- constants.R
+++ constants.R
@@ -1,7 +1,7 @@
 footnote <- "'MZ: Monozygotic, DZ: Dizygotic, DCQ: Dysmorphic concerns questionnaire, MPVS: Multidimensional peer victimization scale, y: year(s)
     ***: p-value <=0.001, **: 0.001 < p-value <= 0.01, *: 0.01 < p-value <= 0.05'"
 footnote_acronyms <- "'MZ: Monozygotic, DZ: Dizygotic, DCQ: Dysmorphic concerns questionnaire, MPVS: Multidimensional peer victimization scale, y: year(s)'"
-footnote_acronyms_2 <- "n refers to the number of individuals, MZ: Monozygotic, DZ: Dizygotic, DCQ: Dysmorphic concerns questionnaire, MPVS: Multidimensional peer victimization scale, y: year(s)"
+footnote_acronyms_2 <- "n refers to the number of individuals, MZ: Monozygotic, DZ: Dizygotic, DCQ: Dysmorphic concerns questionnaire, MPVS: Multidimensional peer victimization scale, y: year(s). At 16y MPVS had 6 items, whereas a 16-item version was used at all other waves."
 
 
 var_labels <- list(
