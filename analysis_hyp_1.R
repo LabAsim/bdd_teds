e5d2bf5b470034e48613702aa07559779cded2c3
LabAsim <lampgit@pm.me>
2025-12-03 11:50:25 +0200

    comment out unnecessary covid model

--- analysis_hyp_1.R
+++ analysis_hyp_1.R
@@ -268,55 +268,55 @@ summary(fit_ml_scaled_32_without_covid)
 # dim(test[complete.cases(test), ])
 
 # With covid
-model_scaled_32_with_covid <- "
-    # DCQ
-    dcq_total_26_1 ~ mpvs_total_12_1_scaled_32
-    dcq_total_26_1 ~ mpvs_total_child_14_1_scaled_32
-    dcq_total_26_1 ~ mpvs_total_16_1_scaled_32
-    dcq_total_26_1 ~ mpvs_total_phase_2_21_1_scaled_32
-    dcq_total_26_1 ~ mpvs_total_cov1_21_1_scaled_32
-    dcq_total_26_1 ~ mpvs_total_cov2_21_1_scaled_32
-    dcq_total_26_1 ~ mpvs_total_cov3_21_1_scaled_32
-    dcq_total_26_1 ~ mpvs_total_cov4_21_1_scaled_32
-
-    # Mpvs
-    mpvs_total_child_14_1_scaled_32 ~ mpvs_total_12_1_scaled_32
-    mpvs_total_16_1_scaled_32 ~ mpvs_total_child_14_1_scaled_32
-    mpvs_total_phase_2_21_1_scaled_32 ~ mpvs_total_16_1_scaled_32
-    mpvs_total_cov1_21_1_scaled_32 ~ mpvs_total_phase_2_21_1_scaled_32
-    mpvs_total_cov2_21_1_scaled_32 ~ mpvs_total_cov1_21_1_scaled_32
-    mpvs_total_cov3_21_1_scaled_32 ~ mpvs_total_cov2_21_1_scaled_32
-    mpvs_total_cov4_21_1_scaled_32 ~ mpvs_total_cov3_21_1_scaled_32
-
-    # Age
-    dcq_total_26_1 ~ age_26_1
-    mpvs_total_12_1_scaled_32 ~ age_child_12_1
-    mpvs_total_child_14_1_scaled_32 ~ age_child_14_1
-    mpvs_total_16_1_scaled_32 ~ age_child_web_16_1
-    mpvs_total_phase_2_21_1_scaled_32 ~ age_phase2_child_21_1
-    mpvs_total_cov2_21_1_scaled_32 ~ age_cov1_child_21_1
-    mpvs_total_cov3_21_1_scaled_32 ~ age_cov2_child_21_1
-    mpvs_total_cov3_21_1_scaled_32 ~ age_cov3_child_21_1
-    mpvs_total_cov4_21_1_scaled_32 ~ age_cov4_child_21_1
-
-    # Sex
-    dcq_total_26_1 ~ sex_1
-    mpvs_total_12_1_scaled_32 ~ sex_1
-    mpvs_total_child_14_1_scaled_32 ~ sex_1
-    mpvs_total_16_1_scaled_32 ~ sex_1
-    mpvs_total_phase_2_21_1_scaled_32 ~ sex_1
-    mpvs_total_cov2_21_1_scaled_32 ~ sex_1
-    mpvs_total_cov3_21_1_scaled_32 ~ sex_1
-    mpvs_total_cov3_21_1_scaled_32 ~ sex_1
-    mpvs_total_cov4_21_1_scaled_32 ~ sex_1
-"
-fit_fiml_scaled_32_with_covid <- sem(
-  model = model_scaled_32_with_covid,
-  data = df_essential_vars,
-  cluster = "fam_id",
-  missing = "fiml"
-)
-summary(fit_fiml_scaled_32_with_covid)
+# model_scaled_32_with_covid <- "
+#     # DCQ
+#     dcq_total_26_1 ~ mpvs_total_12_1_scaled_32
+#     dcq_total_26_1 ~ mpvs_total_child_14_1_scaled_32
+#     dcq_total_26_1 ~ mpvs_total_16_1_scaled_32
+#     dcq_total_26_1 ~ mpvs_total_phase_2_21_1_scaled_32
+#     dcq_total_26_1 ~ mpvs_total_cov1_21_1_scaled_32
+#     dcq_total_26_1 ~ mpvs_total_cov2_21_1_scaled_32
+#     dcq_total_26_1 ~ mpvs_total_cov3_21_1_scaled_32
+#     dcq_total_26_1 ~ mpvs_total_cov4_21_1_scaled_32
+#
+#     # Mpvs
+#     mpvs_total_child_14_1_scaled_32 ~ mpvs_total_12_1_scaled_32
+#     mpvs_total_16_1_scaled_32 ~ mpvs_total_child_14_1_scaled_32
+#     mpvs_total_phase_2_21_1_scaled_32 ~ mpvs_total_16_1_scaled_32
+#     mpvs_total_cov1_21_1_scaled_32 ~ mpvs_total_phase_2_21_1_scaled_32
+#     mpvs_total_cov2_21_1_scaled_32 ~ mpvs_total_cov1_21_1_scaled_32
+#     mpvs_total_cov3_21_1_scaled_32 ~ mpvs_total_cov2_21_1_scaled_32
+#     mpvs_total_cov4_21_1_scaled_32 ~ mpvs_total_cov3_21_1_scaled_32
+#
+#     # Age
+#     dcq_total_26_1 ~ age_26_1
+#     mpvs_total_12_1_scaled_32 ~ age_child_12_1
+#     mpvs_total_child_14_1_scaled_32 ~ age_child_14_1
+#     mpvs_total_16_1_scaled_32 ~ age_child_web_16_1
+#     mpvs_total_phase_2_21_1_scaled_32 ~ age_phase2_child_21_1
+#     mpvs_total_cov2_21_1_scaled_32 ~ age_cov1_child_21_1
+#     mpvs_total_cov3_21_1_scaled_32 ~ age_cov2_child_21_1
+#     mpvs_total_cov3_21_1_scaled_32 ~ age_cov3_child_21_1
+#     mpvs_total_cov4_21_1_scaled_32 ~ age_cov4_child_21_1
+#
+#     # Sex
+#     dcq_total_26_1 ~ sex_1
+#     mpvs_total_12_1_scaled_32 ~ sex_1
+#     mpvs_total_child_14_1_scaled_32 ~ sex_1
+#     mpvs_total_16_1_scaled_32 ~ sex_1
+#     mpvs_total_phase_2_21_1_scaled_32 ~ sex_1
+#     mpvs_total_cov2_21_1_scaled_32 ~ sex_1
+#     mpvs_total_cov3_21_1_scaled_32 ~ sex_1
+#     mpvs_total_cov3_21_1_scaled_32 ~ sex_1
+#     mpvs_total_cov4_21_1_scaled_32 ~ sex_1
+# "
+# fit_fiml_scaled_32_with_covid <- sem(
+#   model = model_scaled_32_with_covid,
+#   data = df_essential_vars,
+#   cluster = "fam_id",
+#   missing = "fiml"
+# )
+# summary(fit_fiml_scaled_32_with_covid)
 
 ############
 # Using MI #
