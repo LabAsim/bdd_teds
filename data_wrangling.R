fc5928685efd5d280e4298daa62406631284338d
LabAsim <lampgit@pm.me>
2026-02-27 20:59:04 +0200

    fix dfs

--- data_wrangling.R
+++ data_wrangling.R
@@ -552,7 +552,7 @@ df <- df[df$aperinat == 0, ]
 
 # The excluded twins
 sprintf(
-  "Excluded %d twins", NROW(df) - NROW(df[df$exclude2 == 0 & df$exclude1 == 0, ])
+  "Excluded %d twins", NROW(df_raw) - NROW(df[df$exclude2 == 0 & df$exclude1 == 0, ])
 )
 # NROW(df[df$sexzyg == 7 | df$acontact == 0 | df$aperinat == 1, ] )
 # We don't have the medexcluded,
